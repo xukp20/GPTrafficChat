4c21ada660365bc08ac280eb082ff5bc976048ff
Kangping Xu <xukp20@mails.tsinghua.edu.cn>
2023-12-13 11:22:55 +0800

    update: add system prompt and clear button for better usage

--- chat.py
+++ chat.py
@@ -7,11 +7,16 @@ MAX_TOKENS=2048
 
 st.title("GPTraffic Chat")
 
-
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-4-1106-preview"
 
 if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Create sidebar for setting prompt
+st.sidebar.title("Settings")
+system_prompt = st.sidebar.text_area("System Prompt", value="")
+if st.sidebar.button("Clear Chat"):
     st.session_state.messages = []
 
 for message in st.session_state.messages:
@@ -25,6 +30,11 @@ if prompt := st.chat_input("What is up?"):
 
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            response = gpt_call(st.session_state.messages, MAX_TOKENS, model=st.session_state.openai_model)
+            all_messages = [{
+                "role": "system",
+                "content": system_prompt
+            }] + st.session_state.messages
+            print(all_messages)
+            response = gpt_call(all_messages, MAX_TOKENS, model=st.session_state.openai_model)
         st.markdown(response)
     st.session_state.messages.append({"role": "assistant", "content": response})
