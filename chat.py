b6e9ff09872a995edc86ccb271e93f7379eb6e86
Kangping Xu <xukp20@mails.tsinghua.edu.cn>
2023-12-14 21:06:01 +0800

    update: add inout token counting, status presenting and cost calculating

--- chat.py
+++ chat.py
@@ -2,8 +2,11 @@
     Built with the basic demo of streamlit chat app
 """
 import streamlit as st
-from gpt import gpt_call
-import json
+from gpt import gpt_call, TOKEN_LOG_ENABLE, money_calculator
+if TOKEN_LOG_ENABLE:
+    from gpt import num_tokens_from_messages, single_text_tokens
+
+import json, time
 MAX_TOKENS=2048
 
 st.title("GPTraffic Chat")
@@ -13,6 +16,29 @@ if "openai_model" not in st.session_state:
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
+
+# put a bar for status below the messages and above the input 
+def status_bar(input_messages, output_str, time):
+    model = st.session_state["openai_model"]
+    if TOKEN_LOG_ENABLE:
+        input_tokens = num_tokens_from_messages(input_messages, model=model)
+        output_tokens = single_text_tokens(output_str, model=model)
+        # keep time to 2 decimal places
+        time = round(time, 2)
+        # calculate money
+        money = money_calculator(input_tokens, output_tokens, model=model)
+        # to string, if > 0.1, use $, else use 
+        if money > 0.1:
+            money = "$" + str(round(money, 2))
+        else:
+            money = str(round(money * 100, 2)) + "¢"
+
+        # token per second, to 2 decimal places
+        tps = round(output_tokens / time, 2)
+
+        st.caption("input: {}, output: {}, time: {}s, tps: {}, cost: {}".format(input_tokens, output_tokens, time, tps, money))
+    else:
+        st.caption(f"time: {round(time, 2)}s")
 
 # Create sidebar for setting prompt
 st.sidebar.title("Settings")
@@ -42,6 +68,7 @@ if prompt := st.chat_input("What is up?"):
 
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
+            start_time = time.time()
             all_messages = [{
                 "role": "system",
                 "content": system_prompt
@@ -52,5 +79,8 @@ if prompt := st.chat_input("What is up?"):
             print("Using model: ", st.session_state.openai_model)
             response = gpt_call(all_messages, MAX_TOKENS, model=st.session_state.openai_model)
             print(response)
+            time_taken = time.time() - start_time
         st.markdown(response)
+
+    status_bar(all_messages, response, time_taken)
     st.session_state.messages.append({"role": "assistant", "content": response})
--- gpt.py
+++ gpt.py
@@ -112,3 +112,61 @@ def single_gpt_call(
         }
     ]
     return gpt_call(messages, max_tokens, temperature, top_p, frequency_penalty, presence_penalty, seed, model)
+
+
+### Tools
+TOKEN_LOG_ENABLE = False
+try:
+    import tiktoken
+    TOKEN_LOG_ENABLE = True
+    def num_tokens_from_messages(messages, model="gpt-4"):
+        """Returns the number of tokens used by a list of messages."""
+        try:
+            encoding = tiktoken.encoding_for_model(model)
+        except KeyError:
+            encoding = tiktoken.get_encoding("cl100k_base")
+        if model == "gpt-3.5-turbo":
+            return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301")
+        elif model == "gpt-4" or model == "gpt-4-1106-preview":
+            return num_tokens_from_messages(messages, model="gpt-4-0314")
+        elif model == "gpt-3.5-turbo-0301":
+            tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
+            tokens_per_name = -1  # if there's a name, the role is omitted
+        elif model == "gpt-4-0314":
+            tokens_per_message = 3
+            tokens_per_name = 1
+        else:
+            raise NotImplementedError(f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens.""")
+        num_tokens = 0
+        for message in messages:
+            num_tokens += tokens_per_message
+            for key, value in message.items():
+                num_tokens += len(encoding.encode(value))
+                if key == "name":
+                    num_tokens += tokens_per_name
+        num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
+        return num_tokens
+
+
+    def single_text_tokens(text, model="gpt-4"):
+        """Returns the number of tokens used by a single text."""
+        try:
+            encoding = tiktoken.encoding_for_model(model)
+        except KeyError:
+            encoding = tiktoken.get_encoding("cl100k_base")
+        return len(encoding.encode(text))
+        
+except ImportError:
+    print("Warning: tiktoken not found, token logger disabled.")
+
+
+def money_calculator(input_tokens, output_tokens, model="gpt-4-1106-preview"):
+    """Returns the money spent on a conversation."""
+    if model == "gpt-3.5-turbo":
+        return input_tokens / 1000 * 0.001 + output_tokens / 1000 * 0.002
+    elif model == "gpt-4-1106-preview":
+        return input_tokens / 1000 * 0.01 + output_tokens / 1000 * 0.03
+    elif model == "gpt-4-32k":
+        return input_tokens / 1000 * 0.06 + output_tokens / 1000 * 0.12
+    else:
+        raise NotImplementedError(f"""money_calculator() is not implemented for model {model}.""")
