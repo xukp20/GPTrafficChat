ca8c088cde91af3a5fc66f7e5acb22e2a220706d
Kangping Xu <xukp20@mails.tsinghua.edu.cn>
2023-12-18 22:11:15 +0800

    update

--- chat.py
+++ chat.py
@@ -7,7 +7,6 @@ if TOKEN_LOG_ENABLE:
     from gpt import num_tokens_from_messages, single_text_tokens
 
 import json, time
-MAX_TOKENS=2048
 
 st.title("GPTraffic Chat")
 
@@ -54,8 +53,40 @@ st.session_state.openai_model = st.sidebar.selectbox(
 )
 
 system_prompt = st.sidebar.text_area("System Prompt", value="")
-if st.sidebar.button("Clear Chat"):
-    st.session_state.messages = []
+# add scroll bars for temperature, top_p, frequency_penalty, presence_penalty and max_tokens
+st.sidebar.subheader("Parameters")
+temperature = st.sidebar.slider("Temperature", 0.0, 2.0, 0.0)
+top_p = st.sidebar.slider("Top P", 0.0, 1.0, 1.0)
+frequency_penalty = st.sidebar.slider("Frequency Penalty", 0.0, 1.0, 0.0)
+presence_penalty = st.sidebar.slider("Presence Penalty", 0.0, 1.0, 0.0)
+max_tokens = st.sidebar.slider("Max Tokens", 1, 2048, 512)
+
+# three columns
+col1, col2, col3 = st.columns(3)
+with col1:
+    if st.sidebar.button("Clear Chat"):
+        st.session_state.messages = []
+with col2:
+    if st.sidebar.button("Remove last round"):
+        if len(st.session_state.messages) >= 2:
+            st.session_state.messages = st.session_state.messages[:-2]
+with col3:
+    if st.sidebar.button("Regenerate"):
+        st.session_state.messages = st.session_state.messages[:-1]
+        with st.spinner("Thinking..."):
+            all_messages = [{
+                "role": "system",
+                "content": system_prompt
+            }] + st.session_state.messages
+            print(all_messages)
+            with open("last_messages.json", "w") as f:
+                f.write(json.dumps(all_messages, indent=4))
+            print("Using model: ", st.session_state.openai_model)
+            print("Parameters: ", temperature, top_p, frequency_penalty, presence_penalty, max_tokens)
+            response = gpt_call(all_messages, model=st.session_state.openai_model,
+                max_tokens=max_tokens, temperature=temperature, top_p=top_p,
+                frequency_penalty=frequency_penalty, presence_penalty=presence_penalty)
+        st.session_state.messages.append({"role": "assistant", "content": response})
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
@@ -75,9 +106,13 @@ if prompt := st.chat_input("What is up?"):
             }] + st.session_state.messages
             print(all_messages)
             with open("last_messages.json", "w") as f:
-                f.write(json.dumps(all_messages))
+                f.write(json.dumps(all_messages, indent=4))
             print("Using model: ", st.session_state.openai_model)
-            response = gpt_call(all_messages, MAX_TOKENS, model=st.session_state.openai_model)
+            print("Parameters: ", temperature, top_p, frequency_penalty, presence_penalty, max_tokens)
+            response = gpt_call(all_messages, model=st.session_state.openai_model,
+                max_tokens=max_tokens, temperature=temperature, top_p=top_p,
+                frequency_penalty=frequency_penalty, presence_penalty=presence_penalty)
+
             print(response)
             time_taken = time.time() - start_time
         st.markdown(response)
